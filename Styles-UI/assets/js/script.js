d0332e141129cd777d0d117de563bf15084ab3a0
KingCreatBug <trunghoang.240500@gmail.com>
2024-04-23 12:18:07 +0700

    #2 - KingCreateBug - feature: Commit kids page

--- Styles-UI/assets/js/script.js
+++ Styles-UI/assets/js/script.js
@@ -129,6 +129,33 @@ window.addEventListener("template-loaded", () => {
     });
 });
 
+// Chuyển tab prod kids
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "prod-list-kids__item";
+    const contentsSelector = "prod-container";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer
+                    .querySelector(`.${tabActive}`)
+                    ?.classList.remove(tabActive);
+                tabContainer
+                    .querySelector(`.${contentActive}`)
+                    ?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
+
 /**
  * JS toggle
  *
