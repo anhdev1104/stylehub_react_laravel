a8bff326219a1d18909a5114177367c5f5388d98
KingCreatBug <trunghoang.240500@gmail.com>
2024-04-26 17:02:24 +0700

    #5 - KingCreateBug- Create all product and About us page

--- Styles-UI/assets/js/script.js
+++ Styles-UI/assets/js/script.js
@@ -252,3 +252,12 @@ window.onload = function () {
         };
     }
 };
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("show");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
