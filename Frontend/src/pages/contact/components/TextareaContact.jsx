6908aa5e4ceeedfbbf187e590a48fe23c43fd35a
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-05-03 14:52:42 +0700

    update title homepage

--- Frontend/src/pages/contact/components/TextareaContact.jsx
+++ Frontend/src/pages/contact/components/TextareaContact.jsx
@@ -1,7 +1,7 @@
 const TextareaContact = props => {
   return (
     <textarea
-      className="w-full h-[180px] p-[15px] border border-[#c4d1d0] resize-none text-dark leading-[1.67] placeholder:text-light"
+      className="w-full h-[180px] p-[15px] border border-[#c4d1d0] resize-none text-dark leading-[1.67] placeholder:text-light outline-none"
       {...props}
     ></textarea>
   );
