b3d8a23cea58e6843cf1e5bc84d651dca72223d3
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-05-18 13:08:48 +0700

    #51 - handle update http

--- Frontend/src/services/products.js
+++ Frontend/src/services/products.js
@@ -1,8 +1,10 @@
-import { addProductApi, deleteProductApi, productDetailsApi, productsApi, updatedProductApi } from '../api/products';
+import Http from '@/helpers/http';
+
+const http = new Http();
 
 export const getProducts = async () => {
   try {
-    const { data } = await productsApi();
+    const data = await http.get('/products');
     return data;
   } catch (error) {
     console.log(error);
@@ -11,7 +13,7 @@ export const getProducts = async () => {
 
 export const getProductDetail = async id => {
   try {
-    const { data } = await productDetailsApi(id);
+    const data = await http.get(`/products/${id}`);
     return data;
   } catch (error) {
     console.log(error);
@@ -20,7 +22,11 @@ export const getProductDetail = async id => {
 
 export const addProduct = async newProduct => {
   try {
-    const { data } = await addProductApi(newProduct);
+    const data = await http.post('/products', newProduct, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
     return data;
   } catch (error) {
     console.log(error);
@@ -29,16 +35,20 @@ export const addProduct = async newProduct => {
 
 export const updatedProduct = async (id, productUpdate) => {
   try {
-    const { data } = await updatedProductApi(id, productUpdate);
+    const data = await http.post(`/products/${id}`, productUpdate, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
     return data;
   } catch (error) {
-    console.log();
+    console.log(error);
   }
 };
 
 export const deleteProduct = async id => {
   try {
-    const { data } = await deleteProductApi(id);
+    const data = await http.delete(`products/${id}`);
     return data;
   } catch (error) {
     console.log(error);
