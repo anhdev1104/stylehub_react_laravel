2aa2d9a7dbd0170e3e60f6edd1976e2f78fe5489
Duong <tinhabc3009@gmail.com>
2024-06-15 01:37:34 +0700

    #66 - Duong04 - Responsive Page FAQ

--- Frontend/src/pages/faq/components/Accordion.jsx
+++ Frontend/src/pages/faq/components/Accordion.jsx
@@ -5,7 +5,7 @@ import { AccordionProvider } from '@/contexts/accordionContext';
 const Accordion = ({ heading, children }) => {
   return (
     <AccordionProvider>
-      <div className="relative p-[30px] border border-[green] bg-white faq-container mb-[23px]">
+      <div className="relative p-[30px] border border-[green] bg-white faq-container mb-[23px] max-md:p-[18px]">
         <AccordionHeader>{heading}</AccordionHeader>
         <AccordionContent>{children}</AccordionContent>
       </div>
--- Frontend/src/pages/faq/components/AccordionHeader.jsx
+++ Frontend/src/pages/faq/components/AccordionHeader.jsx
@@ -6,7 +6,7 @@ const AccordionHeader = ({ children }) => {
 
   return (
     <div className="flex items-center justify-between">
-      <h3 className="text-[22px] font-medium leading-[1.45]">{children}</h3>
+      <h3 className="text-[22px] font-medium leading-[1.45] max-md:text-[20px]">{children}</h3>
       <button
         className="font-medium leading-[1.45] w-[46px] h-[46px] relative bg-white border border-green text-[32px] flex items-center justify-center faq-container__btn"
         onClick={handleToggle}
